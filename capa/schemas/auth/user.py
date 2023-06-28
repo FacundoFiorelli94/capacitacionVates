7f2613bdd82100a31172b3d36ecbf13a0b5862e3
Juan Brusasca <juanbrusasca96@gmail.com>
2023-06-28 14:20:24 -0300

    schemas UserCreate

--- capa/schemas/auth/user.py
+++ capa/schemas/auth/user.py
@@ -1,21 +1,11 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 
 class UserOutput(BaseModel):
     usr_email: EmailStr
-
-    class config:
-        orm_mode = True
-
-
-class UserLoginRequest(UserOutput):
-    usr_password: str
-
-
-class CreateUserRequest(UserLoginRequest):
-    usr_address: str
-    usr_zip: int
-    usr_phone: str
+    usr_address: str = Field(regex="^[a-zA-Z0-9\s\-\#\.\,]+$")
+    usr_zip: str = Field(regex="^\d{5}(-\d{4})?$")
+    usr_phone: str = Field(regex="^\+?\d{1,3}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}$")
     usr_country_id: int
     usr_language_id: int
     usr_enabled: bool = False
@@ -23,3 +13,21 @@ class CreateUserRequest(UserLoginRequest):
 
     class config:
         orm_mode = True
+
+
+class UserCreate(UserOutput):
+    usr_password: str = Field(regex="^[a-zA-Z0-9]+(?:\\s[a-zA-Z0-9]+)*$", max_length=30, min_length=8)  # permite letras y numeros, permite espacios entre las palabras pero no al principio o al final de la oracion
+    # usr_password: str = Field(regex='^(?=.*[A-Z])(?=.*[!@#$%^&*()-=_+[\];:"|,.<>/?])[a-zA-Z0-9]+(?:\s[a-zA-Z0-9]+)*$', max_length=30, min_length=8) #permite letras y numeros, permite espacios entre las palabras pero no al principio o al final de la oracion, y ademas exige una letra mayuscula y un caracter especial (no lo probe)
+
+
+# class CreateUserRequest(UserLoginRequest):
+#     usr_address: str
+#     usr_zip: int
+#     usr_phone: str
+#     usr_country_id: int
+#     usr_language_id: int
+#     usr_enabled: bool = False
+#     usr_is_active: bool = True
+
+#     class config:
+#         orm_mode = True
