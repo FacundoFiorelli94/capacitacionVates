4de34be3de23c61b4c7ce5362f89f06403edc6b6
Juan Brusasca <juanbrusasca96@gmail.com>
2023-06-28 12:23:27 -0300

    merge main into juanb

--- capa/schemas/auth/user.py
+++ capa/schemas/auth/user.py
@@ -1,25 +1,25 @@
-
 from pydantic import BaseModel, EmailStr
 
 
 class UserOutput(BaseModel):
-
     usr_email: EmailStr
+
     class config:
         orm_mode = True
 
 
 class UserLoginRequest(UserOutput):
-  usr_password: str
+    usr_password: str
 
-class CreateUserRequest(UserLoginRequest):    
-  usr_address: str
-  usr_zip: int
-  usr_phone: str
-  usr_country_id: int
-  usr_language_id: int
-  usr_enabled: bool = False  
-  usr_is_active: bool = True
-  
-  class config:
-    orm_mode = True
+
+class CreateUserRequest(UserLoginRequest):
+    usr_address: str
+    usr_zip: int
+    usr_phone: str
+    usr_country_id: int
+    usr_language_id: int
+    usr_enabled: bool = False
+    usr_is_active: bool = True
+
+    class config:
+        orm_mode = True
