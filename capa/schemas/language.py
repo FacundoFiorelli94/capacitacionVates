ca44e49c6d99d285a17ca7ad1204fd75f3e5b29f
Franco-Rosales <rosalesfranco17@gmail.com>
2023-06-29 16:02:59 -0300

    schemas y routes language arreglados

--- capa/schemas/language.py
+++ capa/schemas/language.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel
 
 
-class LanguageBase(BaseModel):
-    language_id: int
-
-
-class LanguageOutput(LanguageBase):
+class LanguageCreate(BaseModel):
     language_name: str
+
+
+class Language(LanguageCreate):
+    language_id: int
 
     class Config:
         orm_mode = True
